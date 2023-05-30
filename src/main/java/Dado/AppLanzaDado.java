1d8330c36ea6b0e90f37b1497a1158c58718bedb
phurtado117 <124796700+phurtado117@users.noreply.github.com>
2023-05-29 19:37:34 -0600

    Update AppLanzaDado.java

--- src/main/java/Dado/AppLanzaDado.java
+++ src/main/java/Dado/AppLanzaDado.java
@@ -1,2 +1,36 @@
-package Dado;public class AppLanzaDado {
-}
+package Dado;
+
+import java.util.Random;
+
+public class AppLanzaDado {
+
+
+        public static void main(String[] args) {
+            Random rand = new Random();
+            int[] resultados = new int[100];
+
+
+            for (int i = 0; i < 100; i++) {
+                resultados[i] = rand.nextInt(6) + 1;
+            }
+
+
+            int[] frecuencias = new int[6];
+            for (int i = 0; i < 100; i++) {
+                frecuencias[resultados[i] - 1]++;
+            }
+
+
+            System.out.println("Resultados de los 100 lanzamientos de un dado:");
+            for (int i = 0; i < 100; i++) {
+                System.out.print(resultados[i] + " ");
+            }
+            System.out.println("\n");
+
+            System.out.println("Frecuencia de cada cara:");
+            for (int i = 0; i < 6; i++) {
+                System.out.println("Cara " + (i+1) + ": " + frecuencias[i]);
+            }
+        }
+    }
+
